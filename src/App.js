5ae026023ab285fe999dc9e74a38bdacea0451e1
taiming <taiming@bridgewell.com>
2021-12-08 11:55:34 +0800

    Add elapsed time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function deleteAllCookies() {
+  var cookies = document.cookie.split(";");
+
+  for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
+}
+
+const App = () => {
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    const startTime = localStorage.getItem('startTime');
+    if (startTime) {
+      const nowTime = Date.now(); // 一個代表由經 UNIX 紀元起經過的毫秒數
+      setElapsedTime(nowTime - startTime);
+      localStorage.removeItem('startTime');
+      deleteAllCookies();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>B 網站</h1>
+      <div>{document.URL}</div>
+      <div>{`Elapsed Time: ${elapsedTime} ms`}</div>
+      <button onClick={() => {
+        window.location.reload();
+      }}>Refresh</button>
     </div>
-  );
+  )
 }
 
 export default App;
